d68e7c2cbb880f4b267d6b4edce2226f83cd719d
kszytko <krzysiek.szytko@gmail.com>
2020-05-15 18:21:59 +0200

    Revert "First pull"

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -1,19 +1,7 @@
 #pragma once
 #include <string>
 
-
-
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
-    if(command == "add")        
-        return std::to_string(first + second);
-    if(command == "subtract")   
-        return std::to_string(first - second);
-    if(command == "multiply")   
-        return std::to_string(first * second);
-    if(command == "divide"){ 
-        if(second == 0) return "Division by 0";
-        return std::to_string(first / second);
-    } 
-    return "Invalid data";    
+    return "";
 }
