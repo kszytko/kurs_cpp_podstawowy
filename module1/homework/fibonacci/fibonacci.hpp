4b2cff8f2ba97d51cb58dbbbea5039e5a65ec3c4
kszytko <krzysiek.szytko@gmail.com>
2020-05-15 18:20:57 +0200

    Revert "Commit fibonacci"

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,25 +1,11 @@
 #pragma once
-#include <vector>
 
 int fibonacci_iterative(int sequence) {
-    std::vector<int> fib(sequence + 1);
-
-    fib[0] = 0;
-    fib[1] = 1;
-
-    for (int i = 2; i < sequence + 1; i++){
-        fib[i] = fib[i-1] + fib[i-2];
-    }
-    
-    return fib.back();
+    // TODO: Your implementation goes here
+    return 0;
 }
 
 int fibonacci_recursive(int sequence) {
-    if(sequence <= 1)
-        return sequence;
-    return fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1);
+    // TODO: Your implementation goes here
+    return 0;
 }
-
-
-//0 1 2 3 4 5 6 7
-//0 1 1 2 3 5 8 13
