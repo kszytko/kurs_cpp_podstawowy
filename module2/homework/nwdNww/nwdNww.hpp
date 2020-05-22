a40df1e4e738242630925466a96c6f50e990034d
KS <krzysiek.szytko@gmail.com>
2020-05-22 23:29:48 +0200

    nwdNww finished with test checking

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,24 @@
 #pragma once
+#include <string>
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWD(const int lhs, const int rhs) {
+    int a = abs(lhs), b = abs(rhs), diff = 0;
+
+    if(lhs == 0){ return rhs; }
+    if(rhs == 0){ return lhs; }
+
+    while(b != 0){
+        diff = a % b;
+        a = b;
+        b = diff;
+    }
+    
+    return a;
 }
 
-int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWW(const int lhs, const int rhs) {
+    if(lhs && rhs){
+        return abs(lhs * rhs) / NWD(lhs, rhs);
+    }
+    return 0;
 }
