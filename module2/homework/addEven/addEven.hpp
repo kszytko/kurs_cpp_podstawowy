2eaf4c6acffd516bd280f4cf650de4b0ce2099a9
Nessy90 <nessy0991@gmail.com>
2020-05-24 10:40:44 +0200

    Fixes in addEven

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -5,7 +5,9 @@ int addEven(const std::vector<int>& numbers) {
 
     int result = 0;
     for(const auto& element : numbers){
-        if (element%2 == 0) result += element;
+        if (element%2 == 0){
+	   result += element;
+	}
     }
     return result;
 }
