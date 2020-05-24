05195983f78f526dce500eab4b2cc1addd0631aa
Nessy90 <nessy0991@gmail.com>
2020-05-24 12:36:03 +0200

    format fixes in addEven

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -1,13 +1,14 @@
 #pragma once
 #include <vector>
 
-int addEven(const std::vector<int>& numbers) {
+int addEven(const std::vector<int>& numbers){
 
     int result = 0;
+
     for(const auto& element : numbers){
-        if (element%2 == 0){
-	   result += element;
-	}
+   	 if (element % 2 == 0){
+	 result += element;
+	 }
     }
     return result;
 }
