03be355e5e42d765dd1908ffd8830f78611fcfc0
Nessy90 <nessy0991@gmail.com>
2020-05-24 10:41:36 +0200

    Fixes in maxOfVector

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -3,10 +3,16 @@
 #include <limits>
 
 int maxOfVector(const std::vector<int>& vec) {
+      
+    if (vec.empty()) return 0;
     
     int max = vec[0];
+
     for (const auto& element : vec){
-        if (element > max) max = element;
+
+        if (element > max){ 
+        max = element;
+        }
     }
     return max;
 }
