4adbf82bb51ca7f183e4d3df69f8bd5bb27955e5
kszytko <krzysiek.szytko@gmail.com>
2020-06-04 00:53:59 +0200

    ErrorCode and printing added

--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,2 +1,35 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard
+class ErrorCode{
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordDoesNotMatch
+}
+
+void getErrorMessage(const ErrorCode& error){
+    std::string message;
+
+    switch(error){
+        case ErrorCode::Ok:
+            message = "Ok";
+            break;
+        case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+            message = "PasswordNeedsAtLeastNineCharacters";
+            break;
+        case ErrorCode::PasswordNeedsAtLeastOneNumber:
+            message = "PasswordNeedsAtLeastOneNumber";
+            break;
+        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+            message = "PasswordNeedsAtLeastOneSpecialCharacter";
+            break;
+        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+            message = "PasswordNeedsAtLeastOneUppercaseLetter";
+            break;
+        case ErrorCode::PasswordDoesNotMatch:
+            message = "PasswordDoesNotMatch";
+            break;
+    }
+
+    std::cout << message;
+}
