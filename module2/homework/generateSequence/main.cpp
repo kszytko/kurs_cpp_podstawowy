88183cca73a4fe157b253de99c9c0707d16dd19f
kszytko <krzysiek.szytko@gmail.com>
2020-06-03 22:08:39 +0200

    main.cpp restore

--- module2/homework/generateSequence/main.cpp
+++ module2/homework/generateSequence/main.cpp
@@ -2,7 +2,7 @@
 #include "generateSequence.hpp"
 
 int main() {
-    for (const int element : generateSequence(-3, 0))
+    for (const int element : generateSequence(10, 5))
         std::cout << element << " | ";
 
     return 0;
