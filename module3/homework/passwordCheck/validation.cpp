95db127bf3d270a83e500b67aec98e54871cd7ac
kszytko <krzysiek.szytko@gmail.com>
2020-06-05 18:26:20 +0200

    getErrorMessage definition added

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,2 +1,31 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+std::string getErrorMessage(const ErrorCode& error){
+    switch(error){
+        std::string message;
+        case ErrorCode::Ok:
+            message = "Ok";
+            break;
+        case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+            message = "Password needs at least nine characters"
+            break;
+        case ErrorCode::PasswordNeedsAtLeastOneNumber:
+            message = "Password needs at least one number"
+            break;
+        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+            message = "Password needs at least one special character"
+            break;
+        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+            message = "Password needs at least one uppercase letter"
+            break;
+        case ErrorCode::PasswordsDoesNotMatch:
+            message = "Passwords does not match";
+            break;
+    }
+    return message;
+}
+bool doesPasswordsMatch(const std::string&, const std::string&);
+
+ErrorCode checkPasswordRules(const std::string&);
+
+ErrorCode checkPassword(const std::string&, const std::string&);
