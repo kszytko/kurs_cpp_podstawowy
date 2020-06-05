194d804de39e53e911e6831ac799cb691313c4e6
kszytko <krzysiek.szytko@gmail.com>
2020-06-05 21:42:57 +0200

    Refactoring code

--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,6 +1,8 @@
 #pragma once 
 #include <string>
 
+constexpr size_t MINIMUM_PASSWORD_LENGTH = 9;
+
 enum class ErrorCode{
     Ok,
     PasswordNeedsAtLeastNineCharacters,
@@ -10,7 +12,7 @@ enum class ErrorCode{
     PasswordsDoesNotMatch
 };
 
-std::string getErrorMessage(const ErrorCode&);
+std::string getErrorMessage(const ErrorCode);
 bool doesPasswordsMatch(const std::string&, const std::string&);
 ErrorCode checkPasswordRules(const std::string&);
 ErrorCode checkPassword(const std::string&, const std::string&);
