676f61d226e802de26f0d28553f1bce9a63151bc
kszytko <krzysiek.szytko@gmail.com>
2020-06-05 18:55:26 +0200

    checkPasswordRules definition added with random error

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,22 +1,24 @@
 #include "validation.hpp"
+#include <random>
 
 std::string getErrorMessage(const ErrorCode& error){
+    std::string message;
+
     switch(error){
-        std::string message;
         case ErrorCode::Ok:
             message = "Ok";
             break;
         case ErrorCode::PasswordNeedsAtLeastNineCharacters:
-            message = "Password needs at least nine characters"
+            message = "Password needs at least nine characters";
             break;
         case ErrorCode::PasswordNeedsAtLeastOneNumber:
-            message = "Password needs at least one number"
+            message = "Password needs at least one number";
             break;
         case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
-            message = "Password needs at least one special character"
+            message = "Password needs at least one special character";
             break;
         case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
-            message = "Password needs at least one uppercase letter"
+            message = "Password needs at least one uppercase letter";
             break;
         case ErrorCode::PasswordsDoesNotMatch:
             message = "Passwords does not match";
@@ -28,6 +30,11 @@ bool doesPasswordsMatch(const std::string& password, const std::string& repeated
     return password == repeatedPassword;
 }
 
-ErrorCode checkPasswordRules(const std::string&);
+ErrorCode checkPasswordRules(const std::string& password){
+    std::random_device rd;
+    std::uniform_int_distribution<> distrib(0, 5);
+    return static_cast<ErrorCode>(distrib(rd));
+
+}
 
 ErrorCode checkPassword(const std::string&, const std::string&);
